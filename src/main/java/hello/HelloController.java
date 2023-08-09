0b0476c536c9783803167b6524c03e29ce54a7fa
samxal <samxal@mail.com>
2023-08-09 12:53:58 +0400

    original version

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,9 +8,9 @@ public class HelloController {
 
     @RequestMapping("/")
         public String index() {
-            return "wellcome our website, please use /hello\n";
+            return "wellcome our website, please use /api4\n";
         }
-    @RequestMapping("/hello")
+    @RequestMapping("/api4")
         public String index2() {
 
             return "Hello, World!\n";
